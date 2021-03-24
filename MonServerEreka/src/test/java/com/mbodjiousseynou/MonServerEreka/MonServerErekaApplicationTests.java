f681ad1d66ba01a10e116d2920466a3843fc050b
oussey94 <mbodjiousseynou94@gmail.com>
2021-03-24 00:28:55 +0000

    premier committtttt

---
+++ MonServerEreka/src/test/java/com/mbodjiousseynou/MonServerEreka/MonServerErekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbodjiousseynou.MonServerEreka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonServerErekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
